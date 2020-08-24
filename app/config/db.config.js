6b8cb729f0f2c4c2ddca5bdfb36ce00ac8791ef5
indraarsy <userindra29@gmail.com>
2020-08-24 21:18:22 +0700

    ganti db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "ec2-52-31-94-195.eu-west-1.compute.amazonaws.com",
-    USER: "vebbieoshqhteu",
-    PASSWORD: "52731a3f78b1799fd42ba5e3ae05e9b50b6f55b157a5485e11876c3763881b9f",
-    DB: "d28d16pm6mi18v",
+    HOST: "ec2-46-137-124-19.eu-west-1.compute.amazonaws.com",
+    USER: "yrhqovyxxdzank",
+    PASSWORD: "42a2efeb531d94a04536aebf5b9508729c131a03d657aec5e26caedef10d1454",
+    DB: "d60fj5frb72fbu",
     dialect: "postgres",
     pool: {
         max: 5,
